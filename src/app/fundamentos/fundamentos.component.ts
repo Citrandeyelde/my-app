0805167234ace1ca1829460f05e4c1393aa21db2
Citrandeyelde <adrianrojas100@gmail.com>
2024-08-01 21:05:36 -0500

    boton contador

--- src/app/fundamentos/fundamentos.component.ts
+++ src/app/fundamentos/fundamentos.component.ts
@@ -10,8 +10,14 @@ import { Component } from '@angular/core';
 export class FundamentosComponent {
   titles="hola p#tas"
   contador= 0
+  imagen = "https://dummyimage.com/300"
+  imagenhola= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi99zAnoi5J9MZdXgXHDxdNQL0n2bZFebK2A&s"
   constructor(){
-    setInterval(()=>{this.contador++}, 2000);
+    setInterval(()=>{this.contador++}, 100);
+  }
+
+  resetearContador(){
+    this.contador=0
   }
 
 }
